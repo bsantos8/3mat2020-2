928906acfeca5a5b89db16a1821a1abd701ff512
Brenda Stefany <65685653+bsantos8@users.noreply.github.com>
2020-08-28 01:37:13 -0300

    Update variaveis.js

--- variaveis.js
+++ variaveis.js
@@ -1,8 +1,8 @@
 const x = []
-
+//adiciona novos valores a const
 x.push(4)
 x.push(19)
 
 console.log(x)
-
+//não da para adicionar novo valor a const - tem que usar o push para adicionar novo valor
 x = []
